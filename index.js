34782d0454dc2b3c7cbc3f7e053bddb1cdf55a52
mohinuddinqureshi <sahilkhanmoinisha@gmail.com>
2023-10-21 14:06:40 +0300

    hostapi live

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-
-
 app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-const apiData =require("./data.json");
+const apiData = require("./data.json");
 
-app.get("/",(req,res) => {
-    res.send("hello i am live");
+app.get("/", (req, res) => {
+  res.send("hello i am live");
 });
 
-app.get("/services", (req, res)=>{
-    res.send(apiData);
-})
+app.get("/services", (req, res) => {
+  // Use res.json() instead of res.send() for sending JSON data
+  res.json(apiData);
+});
 
 app.listen(port, () => {
-    console.log("i am live again");
-
+  console.log("i am live again");
 });
